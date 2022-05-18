f6388f1f8b97f90b146de0b5474824482302f976
unknown <96012066+TryToCode433@users.noreply.github.com>
2022-05-18 16:10:49 +0900

    22/5/18.1

---
+++ keyboard.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+class getNum{
+	void key(){
+		
+	}
+}
+
+class keyboard{
+	public static void main(String[] args) throws IOException{
+		System.out.println("put a nummber");
+		BufferedReader keyB = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = keyB.readLine();
+		
+		int num = Integer.parseInt(str);
+		
+		System.out.println("Putted\t" + num );
+	}
+}
