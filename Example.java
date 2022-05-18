0262da9d676632dd49b3f810d19638ba8817c680
unknown <96012066+TryToCode433@users.noreply.github.com>
2022-05-18 15:50:27 +0900

    22/5/18

---
+++ Example.java
@@ -0,0 +1,17 @@
+class Example{
+	public static void main(String[] args){
+		System.out.println("--------------------");
+		double d;
+		d = 32;
+		System.out.println("doubleå^\t" + d);
+		System.out.println("--------------------");
+		boolean b;
+		b = false;
+		System.out.println("booleanå^\t" + b);
+		System.out.println("--------------------");
+		char c;
+		c = 'v';
+		System.out.println("charå^\t\t" + c);
+		System.out.println("--------------------");
+	}
+}
