1bb61d995010bdc136351b58a09c01367ff08c35
unknown <96012066+TryToCode433@users.noreply.github.com>
2022-05-25 16:08:17 +0900

    22/05/25.1

--- keyboard.java
+++ keyboard.java
@@ -1,21 +1,23 @@
 import java.io.*;
 
 class getNum{
-	void key(){
+	public static void key(){
 		
 	}
 }
 
 class keyboard{
 	public static void main(String[] args) throws IOException{
-		System.out.println("put a nummber");
+		System.out.println("Put 2 nummber to get sum!");
 		BufferedReader keyB = new BufferedReader(new InputStreamReader(System.in));
 		
 		String str = keyB.readLine();
+		String str1 = keyB.readLine();
 		
 		int num = Integer.parseInt(str);
+		int num1 = Integer.parseInt(str1);
 //		double doub = Double.parseDouble(str);
 		
-		System.out.println("Putted\t" + num );
+		System.out.println("Sum\t" + (num + num1) );
 	}
 }
